4bd4bd062eb73b1ccffa10e85bc48966bb643c9b
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2024-08-17 22:16:21 +0600

    git

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,6 +7,7 @@ function Navbar() {
     <nav className="bg-muted py-4 sm:px-16 lg:px-24 px-4 flex items-center justify-between">
       <div>
         <LinksDropdown />
+        <p className="">testing git</p>
       </div>
 
       <div className="flex items-center gap-x-4">
